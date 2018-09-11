803af10615576b57b72bc42e5571e45936fca430
Giovanni Gaspar <giovannigaspar@outlook.com>
2018-09-11 20:58:09 -0300

    Some validations

--- src/app/models.py
+++ src/app/models.py
@@ -1,4 +1,6 @@
 from app.db import get_dict_resultset, ONE, ALL
+from flask import jsonify
+from app.tools import validate_params
 
 
 def start_call(js):
@@ -31,7 +33,7 @@ def start_call(js):
         js.get('source'),
         js.get('destination')
     )
-    return get_dict_resultset(sql, params, ONE)
+    return jsonify(get_dict_resultset(sql, params, ONE))
 
 
 def end_call(js):
@@ -40,6 +42,8 @@ def end_call(js):
     Since inconsistencies can happen, the code will try to insert if the
     "call_id" is not yet present.
     """
+
+    validate_params(['call_id', 'timestamp'], js)
 
     sql = '''
         INSERT INTO CALLS (
@@ -56,12 +60,21 @@ def end_call(js):
 
         js.get('timestamp')
     )
-    return get_dict_resultset(sql, params, ONE)
+    return jsonify(get_dict_resultset(sql, params, ONE))
 
 
 def get_bill(phone_number):
     # ToDo -> Just a placeholder
     sql = '''
-        SELECT * FROM CALLS
+        SELECT
+            timestamp_begin,
+            timestamp_end,
+            (timestamp_end - timestamp_begin) as duration
+        FROM CALLS
+        WHERE (
+            (timestamp_end IS NOT NULL) AND
+            (timestamp_begin IS NOT NULL) AND
+            (timestamp_end > timestamp_begin)
+        )
     '''
     return get_dict_resultset(sql, None, ALL)
--- src/app/routes.py
+++ src/app/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request, abort, Blueprint
+from flask import request, abort, Blueprint
 from app.models import start_call, end_call, get_bill
 
 
@@ -8,8 +8,21 @@ bp_routes = Blueprint('routes', __name__)
 @bp_routes.route('/call', methods=['POST'])
 def call_record():
     """
-    Route used to start/end a call. Receives a JSON contaning
+    Route used to start/end a call.
+    Receives a JSON contaning the following information:
+
+        "type":  Indicate if it's a call "start" or "end" record;
+        "timestamp":  The timestamp of when the event occured;
+        "call_id":  Unique for each call record pair;
+        "source"*:  The subscriber phone number that originated the call;
+        "destination"*:  The phone number receiving the call.
+
+    * Not required for the type "end".
+
+    If any of the required items is not present, return a HTTP response status
+    code 500 contaning a custom message.
     """
+
     js = request.get_json()
     call_type = js.get('type', '')
     r = None
@@ -17,7 +30,7 @@ def call_record():
         r = start_call(js)
     elif (call_type == 'end'):
         r = end_call(js)
-    return jsonify(objects=r) if r else abort(500)
+    return r if r else abort(500, "Invalid JSON")
 
 
 @bp_routes.route('/phone/<phone_number>/bill', methods=['GET'])
