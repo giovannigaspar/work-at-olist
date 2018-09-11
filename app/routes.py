b232890ff962bfc4e0bfb6b51266c487797ffa2e
Giovanni Gaspar <giovannigaspar@outlook.com>
2018-09-11 10:07:47 -0300

    Changes in routes' syntax

--- app/routes.py
+++ app/routes.py
@@ -5,16 +5,18 @@ from app.models import start_call, end_call, get_bill
 bp_routes = Blueprint('routes', __name__)
 
 
-@bp_routes.route('/call/<type>', methods=['POST'])
-def call_record(type):
-    if (type == 'start'):
-        start_call(request.get_json())
-    elif (type == 'end'):
-        end_call(request.get_json())
+@bp_routes.route('/call', methods=['POST'])
+def call_record():
+    js = request.get_json()
+    call_type = js.get('type', '')
+    if (call_type == 'start'):
+        start_call(js)
+    elif (call_type == 'end'):
+        end_call(js)
     abort(404)
 
 
-@bp_routes.route('/phone/<int: phone_number>/bill', methods=['GET'])
+@bp_routes.route('/phone/<phone_number>/bill', methods=['GET'])
 def get_phone_bill(phone_number):
     # ToDo: Need to insert period!
     get_bill(phone_number)
