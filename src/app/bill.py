a5cb048b9e373cc1f4d2f3f93ecea49eaf0c97ec
Giovanni Gaspar <giovannigaspar@outlook.com>
2018-09-12 14:44:39 -0300

    Fixes on method to calculate the bill

--- src/app/bill.py
+++ src/app/bill.py
@@ -5,13 +5,22 @@ STANDING_CHARGE = 0.36
 DAY_TAX = 0.09
 
 
-def calculate_call_tariff(begin, end):
+def _calculate_call_tariff(begin, end, duration):
+    """
+    Calculate the call tariff based on the call start, end and duration.
+
+    :param begin: Start of call timestamp.
+    :param end: End of call timestamp.
+    :param duration: Duration of call in timedelta format
+    """
     current_time = begin
     minutes = 0
     price = STANDING_CHARGE
     while (current_time < end):
         current_time = current_time + datetime.timedelta(0,60)
         if (current_time.hour >= 6) and (current_time.hour < 22):
-            minutes = minutes+1
+            minutes = (minutes+1)
+    if (str(duration).split(':')[2] != '00'):
+        minutes = (minutes-1)
     price = price + (minutes * DAY_TAX)
-    print(price)
+    return str(round(price, 2))
