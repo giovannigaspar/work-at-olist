3bba7c990c4e00e626e82d358d39165a3874ba1c
Giovanni Gaspar <giovannigaspar@outlook.com>
2018-09-11 22:16:51 -0300

    Custom JSON encoder for datetime

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -6,11 +6,27 @@ __status__ = "Development"
 
 
 from flask import Flask
+from flask.json import JSONEncoder
+from datetime import timedelta, datetime
 from app import config
 from app.routes import bp_routes
 
 
+# Custom JSON encoder, since, by default, FLASK won't decode a datetime as ISO
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            #if isinstance(obj, timedelta):
+            #    return str(obj)
+            if isinstance(obj, datetime):
+                return obj.isoformat()
+        except TypeError:
+            pass
+        return super().default(obj)
+
+
 app = Flask(__name__) # Flask default syntax
+app.json_encoder = CustomJSONEncoder
 app.config.from_object(config) # Getting APP configs from file
 app.jinja_env.cache = {} # Disabling some caches for better performance
 
