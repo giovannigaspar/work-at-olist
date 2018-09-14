32497a17a4fbb2b40f781ecf31f0140213d3c94a
Giovanni Gaspar <giovannigaspar@outlook.com>
2018-09-14 17:21:49 -0300

    API Examples -> Listing Bill

--- src/app/static/js/examples.js
+++ src/app/static/js/examples.js
@@ -13,6 +13,40 @@ function Example()
                 $('#callDestination').val('');
             }
         });
+
+        $('#btnGetBill').on('click', function() {
+            ex.onBtnSendDataClick();
+        });
+    };
+
+    this.onBtnSendDataClick = function() {
+        req.getJSONRequest(
+            '/phone/99988526423/bill?period=12/2017',
+            this.sendDataCallBack, null, this
+        );
+    };
+    this.sendDataCallBack = function(data, self) {
+        if (data !== 'error') {
+            let $tableBody = $('#tableBody');
+            $tableBody.html('');
+
+            let content = '';
+            for (const key in data) {
+                console.log(data[key]);
+
+                content +=
+                    '<tr>' +
+                        '<td>' + data[key]['destination'] + '</td>' +
+                        '<td>' + data[key]['call_start_date'] + '</td>' +
+                        '<td>' + data[key]['call_start_time'] + '</td>' +
+                        '<td>' + data[key]['duration'] + '</td>' +
+                        '<td>' + data[key]['call_price'].replace('.', ',') + '</td>' +
+                    '</tr>';
+            }
+            $tableBody.append(content);
+        } else {
+            // ToDo
+        }
     };
 }
 
